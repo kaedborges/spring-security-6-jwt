35effbfd80de17c7c9bf22a31c1874c59a3944b1
Edvander Borges <edvander.borges@nextbss.co.ao>
2023-08-02 20:48:06 +0100

    update register request

--- src/main/java/com/kaedkode/coursesecurity/auth/RegisterRequest.java
+++ src/main/java/com/kaedkode/coursesecurity/auth/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.kaedkode.coursesecurity.auth;
 
+import com.kaedkode.coursesecurity.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,4 +16,5 @@ public class RegisterRequest {
   private String lastname;
   private String email;
   private String password;
+  private Role role;
 }
