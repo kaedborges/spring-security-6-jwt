9a54c98767b53f2c279f2fdfd3ea3e19a48bed20
Edvander Borges <edvander.borges@nextbss.co.ao>
2023-08-02 20:49:28 +0100

    update authentication service adding new feature

--- src/main/java/com/kaedkode/coursesecurity/auth/AuthenticationService.java
+++ src/main/java/com/kaedkode/coursesecurity/auth/AuthenticationService.java
@@ -1,20 +1,30 @@
 package com.kaedkode.coursesecurity.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kaedkode.coursesecurity.config.JwtService;
+import com.kaedkode.coursesecurity.token.Token;
+import com.kaedkode.coursesecurity.token.TokenRepository;
+import com.kaedkode.coursesecurity.token.TokenType;
 import com.kaedkode.coursesecurity.user.Role;
 import com.kaedkode.coursesecurity.user.User;
 import com.kaedkode.coursesecurity.user.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-  private final UserRepository repository;
+  private final UserRepository userRepository;
+  private final TokenRepository tokenRepository;
   private final PasswordEncoder passwordEncoder;
   private final JwtService jwtService;
   private final AuthenticationManager authenticationManager;
@@ -26,11 +36,16 @@ public class AuthenticationService {
             .lastname(request.getLastname())
             .emial(request.getEmail())
             .password(passwordEncoder.encode(request.getPassword()))
-            .role(Role.USER)
+            .role(request.getRole())
             .build();
-    repository.save(user);
+    User savedsave = userRepository.save(user);
     var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder().token(jwtToken).build();
+    var refreshToken = jwtService.generateRefreshToken(user);
+    saveUserToken(savedsave, jwtToken);
+    return AuthenticationResponse.builder()
+            .accessToken(jwtToken)
+            .refreshToken(refreshToken)
+            .build();
   }
 
   public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -40,9 +55,68 @@ public class AuthenticationService {
                     request.getPassword()
             )
     );
-    var user = repository.findByEmial(request.getEmail())
+    var user = userRepository.findByEmial(request.getEmail())
             .orElseThrow();
     var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder().token(jwtToken).build();
+    var refreshToken = jwtService.generateRefreshToken(user);
+    revokeAllUserTokens(user);
+    saveUserToken(user, jwtToken);
+    return AuthenticationResponse.builder()
+            .accessToken(jwtToken)
+            .refreshToken(refreshToken)
+            .build();
+  }
+
+  private void saveUserToken(User user, String jwtToken){
+    var token = Token.builder()
+            .user(user)
+            .token(jwtToken)
+            .tokenType(TokenType.BEARER)
+            .expired(false)
+            .revoked(false)
+            .build();
+    tokenRepository.save(token);
+  }
+
+  private void revokeAllUserTokens(User user){
+    var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+    if(validUserTokens.isEmpty()){
+      return;
+    }
+    validUserTokens.forEach(token -> {
+      token.setRevoked(true);
+      token.setExpired(true);
+    });
+    tokenRepository.saveAll(validUserTokens);
+  }
+
+  public void refreshToken(
+          HttpServletRequest request,
+          HttpServletResponse response
+  ) throws IOException {
+    final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    final String refreshToken;
+    final String userEmail;
+
+    if (authHeader != null || authHeader.startsWith("Bearer "))
+      return;
+
+    refreshToken = authHeader.substring(7);
+    userEmail = jwtService.extractUsername(refreshToken);
+    if (userEmail != null){
+      var user  = this.userRepository.findByEmial(userEmail)
+              .orElseThrow();
+      if (jwtService.isTokenValid(refreshToken,user)){
+        var accessToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, accessToken);
+        var authResponse = AuthenticationResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+        new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+      }
+    }
+
   }
 }
