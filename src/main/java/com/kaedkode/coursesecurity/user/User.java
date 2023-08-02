adf09048165ce80842a706591ae2caf7fd7735d8
Edvander Borges <edvander.borges@nextbss.co.ao>
2023-08-02 18:03:07 +0100

    update user adding token

--- src/main/java/com/kaedkode/coursesecurity/user/User.java
+++ src/main/java/com/kaedkode/coursesecurity/user/User.java
@@ -1,5 +1,6 @@
 package com.kaedkode.coursesecurity.user;
 
+import com.kaedkode.coursesecurity.token.Token;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -21,7 +23,7 @@ public class User implements UserDetails {
 
   @Id
   @GeneratedValue
-  private Integer id;
+  private UUID id;
   private String firstname;
   private String lastname;
   private String emial;
@@ -29,6 +31,9 @@ public class User implements UserDetails {
 
   @Enumerated(EnumType.STRING)
   private Role role;
+
+  @OneToMany(mappedBy = "user")
+  private List<Token> tokens;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
